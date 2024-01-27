361a243440e2fa8418a5815588586eb29633a554
PampariNaresh <pamparinaresh2001@gmail.com>
2024-01-28 00:34:27 +0530

    Aplliction.py

--- application.py
+++ application.py
@@ -277,5 +277,6 @@ def abstract():
     return render_template('abstract.html')  # Create an 'abstract.html' template
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)        
+    #app.run(host="0.0.0.0",debug=True) 
+    app.run()        
 
